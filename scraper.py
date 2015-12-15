1f5cd788cd172f6b30c91155d4926db54a0f582e
woodbine <ian@spendnetwork.com>
2015-12-15 04:20:42 +0000

    Set unique key to URL

--- scraper.py
+++ scraper.py
@@ -120,7 +120,7 @@ for row in data:
     valid = validate(filename, file_url, requestdata)
 
     if valid == True:
-        scraperwiki.sqlite.save(unique_keys=['f'], data={"l": file_url, "f": filename, "d": todays_date })
+        scraperwiki.sqlite.save(unique_keys=['l'], data={"l": file_url, "f": filename, "d": todays_date })
         print filename
     else:
         errors += 1
